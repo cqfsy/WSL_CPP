4d32caa6032f1bc9d037407950c80c8a3cdaf2d2
Scofield <fengshenyao@foxmail.com>
2022-06-25 11:59:31 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+
+using namespace std;
+
+
+int count() {
+    int a = 0;
+    static int b = 0;
+    ++a;
+    ++b;
+    cout << "a: " << a << ", b: " << b << endl;
+    return 0;
+}
+
+
+int main() {
+    cout << "这是一个说明【静态变量】的例子" << endl;
+    for (int i = 0; i < 3; ++i) {
+        count();
+    }
+    return 0;
+}
